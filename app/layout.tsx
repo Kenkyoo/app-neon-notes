22ac6ffe219f9db4b75dfe26758db0b01b05e067
Franco <frfantusatti@protonmail.com>
2025-11-07 15:04:16 -0300

    add sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
-"use client"
-
 import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackServerApp } from "./stack";
 import "@/app/styles/globals.css";
 import { Provider } from "@/components/ui/provider";
-import { ColorModeProvider } from "@/components/ui/color-mode";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 export default function RootLayout({
   children,
@@ -17,11 +13,7 @@ export default function RootLayout({
       <body>
         <StackProvider app={stackServerApp}>
           <StackTheme>
-            <ColorModeProvider>
-              <ChakraProvider value={defaultSystem}>
                 <Provider>{children}</Provider>
-              </ChakraProvider>
-            </ColorModeProvider>
           </StackTheme>
         </StackProvider>
       </body>
